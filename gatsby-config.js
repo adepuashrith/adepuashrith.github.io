398af577ca473bfced85fd92822719cb78b87222
Ashrith Adepu <60426256+ashrithadepu27@users.noreply.github.com>
2024-09-26 00:02:33 +0000

    Added link to domain in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `Ashrith Adepu`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    pathPrefix: "/ashrithadepu27.github.io",
   },
   flags: {
     DEV_SSR: true
